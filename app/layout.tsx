a56d3dacc329e3f4f7bf8d224b3806a134571cc7
sergiopitstop1-png <sergiopitstop1@gmail.com>
2026-03-31 11:02:36 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "🔥 Profit Tracker",
+  title: "🔥 Sergio Apicella",
   description: "Gestione avanzata profitti",
 };
 
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
      <body className="min-h-full flex flex-col">
-  <Navbar />
+ 
   {children}
 </body>
     </html>
