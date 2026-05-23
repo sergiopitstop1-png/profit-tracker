78e15cc20d785df8347699dd619eef664cd4e81f
sergiopitstop1-png <sergiopitstop1@gmail.com>
2026-05-23 18:37:13 +0200

    Update page.tsx

--- app/profit-tracker/page.tsx
+++ app/profit-tracker/page.tsx
@@ -1,11 +1,5 @@
-"use client";
 import ProfitTrackerClient from "../components/ProfitTrackerClient";
-import AuthGuard from "../components/AuthGuard";
 
 export default function ProfitTrackerPage() {
-  return (
-    <AuthGuard>
-      <ProfitTrackerClient />
-    </AuthGuard>
-  );
+  return <ProfitTrackerClient />;
 }
