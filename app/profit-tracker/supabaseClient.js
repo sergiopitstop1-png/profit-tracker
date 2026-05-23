75966b5c81001b6bb94627807170adb4473f9370
sergiopitstop1-png <sergiopitstop1@gmail.com>
2026-05-23 19:09:06 +0200

    Update supabaseClient.js

--- app/profit-tracker/supabaseClient.js
+++ app/profit-tracker/supabaseClient.js
@@ -1,5 +1,6 @@
-"use client";
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from "../../lib/supabase-browser";
-
-export const supabase = createClient();
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
