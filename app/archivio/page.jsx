a0e9968673bc84296c235089e2fabe341d80958d
sergiopitstop1-png <sergiopitstop1@gmail.com>
2026-08-12 16:24:27 +0200

    Update page.jsx

--- app/archivio/page.jsx
+++ app/archivio/page.jsx
@@ -1,12 +1,9 @@
 "use client";
 import AuthGuard from "../components/AuthGuard";
 import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "../lib/supabase-browser";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+const supabase = createClient();
 
 const API_FD = "/api/footballdata";
 
