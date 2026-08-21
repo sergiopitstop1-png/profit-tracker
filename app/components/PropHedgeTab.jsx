79c03162561f6306ec5e034bb070e903d72e9997
sergiopitstop1-png <sergiopitstop1@gmail.com>
2026-08-21 09:43:37 +0200

    Update PropHedgeTab.jsx

--- app/components/PropHedgeTab.jsx
+++ app/components/PropHedgeTab.jsx
@@ -2123,7 +2123,7 @@ export default function PropHedgeTab() {
         symbol,
         // Valori neutri: SYMBOL_INFO non li usa.
         side: "BUY",
-        volume: 0,
+        volume: 0.01,
         entry_price: null,
         sl: null,
         tp: null,
